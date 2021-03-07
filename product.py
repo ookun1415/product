c09f1e1be0aae1b98abc9f6284bc9397b344a318
ookun <jeffery12369874@gmail.com>
2021-03-07 12:49:19 +0800

    add product.py

---
+++ product.py
@@ -0,0 +1,13 @@
+product = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	p = []
+	p.append(name)
+	p.append(price) #7到9可以改成 p = [name, price]
+	product.append(p) #7到10可以改成product.append([name, price])
+print(product)
+
+product[0][0] #product清單第0格的第0格
